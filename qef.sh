b33f87d7b8df03a41987cc396001322c51af67e8
Regis Pires Magalhaes <regispiresmag@gmail.com>
2014-10-13 02:54:54 -0300

    qef.sh modified to execute qef via maven

--- qef.sh
+++ qef.sh
@@ -1,17 +1,2 @@
-#! /bin/bash
-# add the libraries to the CLASSPATH.
-ORIGINAL_DIR=$PWD
-APP_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $APP_DIR
-MAIN_CLASS=ch.epfl.codimsd.qeef.QueryManagerImpl
-APP_CLASSPATH=CoDIMS-tcp/build/classes
-DIRLIBS=CoDIMS-tcp/lib/*.jar
-
-for i in ${DIRLIBS}
-do
-  APP_CLASSPATH=$APP_CLASSPATH:"$i"
-done
-
-java -cp "$APP_CLASSPATH:$CLASSPATH" $MAIN_CLASS "$@"
-cd "$ORIGINAL_DIR"
-
+#!/bin/bash
+mvn exec:java -Dexec.mainClass="ch.epfl.codimsd.qeef.QueryManagerImpl"
