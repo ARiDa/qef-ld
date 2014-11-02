987bb812b00490520aeb927487c6d4c4ab062252
Regis Pires Magalhaes <regispiresmag@gmail.com>
2014-11-02 10:06:33 -0300

    updated to new jena version

--- qef.sh
+++ qef.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn exec:java -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="ch.epfl.codimsd.qeef.QueryManagerImpl" -Dexec.args="$1"
+mvn exec:java -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="ch.epfl.codimsd.qeef.QueryManagerImpl" -Dexec.args="$@"
